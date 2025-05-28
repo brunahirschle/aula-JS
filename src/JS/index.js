d5f664231a018715cf2f02da8fc54707db2559e9
bruna <brunaahhirschle@gmail.com>
2025-05-28 18:59:48 -0300

    variaveis

---
+++ src/JS/index.js
@@ -0,0 +1,25 @@
+let num1 = Number(prompt("Digite o primeiro número"));
+num1 *= 10
+let num2 = Number(prompt("Digite o segundo número"));
+num2 += 2
+
+let soma = num1 + num2;
+let diferenca = num1 - num2;
+let produto = num1 * num2;
+let quociente = num1 / num2;
+let resto = num1 % num2;
+
+alert("A soma dos números é " + soma);
+console.log("A soma dos números é " + soma);
+
+alert("A diferença dos números é " + diferenca);
+console.log("A diferença dos números é " + diferenca);
+
+alert("O produto dos números é " + produto);
+console.log("O produto dos números é " + produto);
+
+alert("O quociente dos números é " + quociente);
+console.log("O quociente dos números é " + quociente);
+
+alert("O resto da dividão dos números é " + resto);
+console.log("O resto da divisão dos números é " + resto);
